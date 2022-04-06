4b79001afa6c35e80d606f9d56f810154c555d7c
Nicholas Cassie <nicholascassie@pop-os.localdomain>
2022-04-06 13:59:04 -0400

    shell for lexer

--- src/Lox.java
+++ src/Lox.java
@@ -5,7 +5,68 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 public class Lox {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException{
+        if(args.length > 1){
+            System.out.println("Usage: jlox [script]");
+        } else if (args.length == 1){
+            runFile(args[0]);
+        } else{
+            runPrompt();
+        }
+    }
+
+    // runs input file
+    private static void runFile(String path) throws IOException{
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        if(hadError){
+            System.exit(65);
+        }
+    }
+
+    // accept input from prompt and execute user input
+    private static void runPrompt() throws IOException{
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.println("> ");
+            String line = reader.readLine();
+            if(line == null){ // CTRL-D triggers null from readline
+                break;
+            }
+            run(line);
+            hadError = false;
+        }
+    }
+
+    // core function to run interpreter
+    private static void run(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // temporary functionality to print tokens
+        for(Token token : tokens){
+            System.out.println(token);
+        }
+    }
+
+    // error handling functions
+    // could be moved to a different class/interface
+    static void error(int line, String message){
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message){
+        System.err.println("[line " + line + "] Error " + where + ": " + message);
+        hadError = true;
+    }
 
 }
+
+
