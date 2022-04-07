4ad5533d7e30bed7e9e02fae9d0fbc43edd65115
Nicholas Cassie <nicholascassie@pop-os.localdomain>
2022-04-07 12:32:53 -0400

    Lexer finished

--- src/Scanner.java
+++ src/Scanner.java
@@ -7,6 +7,27 @@ public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
+    private static final Map<String, TokenType> keywords;
+    static{
+        keywords = new HashMap<>();
+        keywords.put("and", TokenType.AND);
+        keywords.put("class", TokenType.CLASS);
+        keywords.put("else", TokenType.ELSE);
+        keywords.put("false", TokenType.FALSE);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("fun", TokenType.FUN);
+        keywords.put("if", TokenType.IF);
+        keywords.put("nil", TokenType.NIL);
+        keywords.put("or",TokenType.OR);
+        keywords.put("print", TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super", TokenType.SUPER);
+        keywords.put("this", TokenType.THIS);
+        keywords.put("true", TokenType.TRUE);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("while", TokenType.WHILE);
+    }
+
     // offsets to index into string
     private int start = 0;
     private int current = 0;
@@ -29,7 +50,178 @@ public class Scanner {
         return tokens;
     }
 
+    private void scanToken(){
+        char c = advance();
+        switch(c){
+            case '(': addToken(TokenType.LEFT_PAREN); break;
+            case ')': addToken(TokenType.RIGHT_PAREN); break;
+            case '{': addToken(TokenType.LEFT_BRACE); break;
+            case '}': addToken(TokenType.RIGHT_BRACE); break;
+            case ',': addToken(TokenType.COMMA); break;
+            case '.': addToken(TokenType.DOT); break;
+            case '-': addToken(TokenType.MINUS); break;
+            case '+': addToken(TokenType.PLUS); break;
+            case ';': addToken(TokenType.SEMICOLON); break;
+            case '*': addToken(TokenType.STAR); break;
+            case '!':
+                addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+                break;
+            case '=':
+                addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+                break;
+            case '>':
+                addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+            case '/':
+                if(match('/')){
+                    // ignore rest of line due to comment
+                    while(peek() != '\n' && !isAtEnd()){
+                        advance();
+                    }
+                }else{
+                    addToken(TokenType.SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                // ignores whitespace
+                break;
+            case '\n':
+                // need to advance line pointer on newline
+                line++;
+                break;
+            case '"': string(); break;
+            default:
+                if(isDigit(c)){
+                    number();
+                } else if(isAlpha(c)){
+                    identifier();
+                }
+                else{
+                    Lox.error(line, "Unexpected character.");
+                }
+
+                break;
+        }
+    }
+
+    // process string literal
+    // string is likely a bad name - could be more descriptive
+    private void string(){
+        while(peek() != '"' && !isAtEnd()){
+            if(peek() == '\n'){
+                line++;
+            }
+            advance();
+        }
+
+        if(isAtEnd()){
+            Lox.error(line, "Unterminated string");
+            return;
+        }
+
+        // grab the closing "
+        advance();
+        String value = source.substring(start+1, current-1);
+        addToken(TokenType.STRING, value);
+    }
+
+    // process a number literal
+    // again, name could be more descriptive
+    private void number(){
+        while (isDigit(peek())){
+            advance();
+        }
+
+        // see if number has a decimal
+        if(peek()== '.' && isDigit(peekNext())){
+            // eat the '.'
+            advance();
+
+            while(isDigit(peek())){
+                advance();
+            }
+        }
+        Double value = Double.parseDouble(source.substring(start, current));
+        addToken(TokenType.NUMBER, value);
+    }
+
+    // process identifier
+    // name could be more descriptive
+    private void identifier(){
+        while(isAlphaNumeric(peek())){
+            advance();
+        }
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type==null){
+            type = TokenType.IDENTIFIER;
+        }
+        addToken(type);
+    }
+
+    // tests if current character matches expected
+    // used primarily for 2-character length operators (e.g. distinguish = from ==);
+    private boolean match(char expected){
+        if(isAtEnd()){
+            return false;
+        }
+        if(source.charAt(current) != expected){
+            return false;
+        }
+        current++;
+        return true;
+    }
+
+    // helper method to lookahead at next character
+    private char peek(){
+        if(isAtEnd()){
+            return '\0';
+        }
+        return source.charAt(current);
+    }
+
+    // two char lookahead. Used for determining if a number contains a decimal
+    private char peekNext(){
+        if(current + 1 >= source.length()){
+            return '\0';
+        }
+        return source.charAt(current+1);
+    }
+
+    private boolean isAlpha(char c){
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c == '_');
+    }
+
+    private boolean isAlphaNumeric(char c){
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c){
+        return c >= '0' && c <= '9';
+    }
+
     private boolean isAtEnd(){
         return current >= source.length();
+    }
+
+    private char advance(){
+        return source.charAt(current++);
+    }
+
+    // used for tokens that don't have a literal value
+    // fills in null for literal in overloaded method
+    private void addToken(TokenType type){
+        addToken(type, null);
+    }
+
+    // used to create tokens with literal values
+    private void addToken(TokenType type, Object literal){
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 }
